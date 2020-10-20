f128abe4f6ac20c2290cafec1a6eeec758191adf
Colin J McRae <cjmcrae@me.com>
2020-10-20 17:54:46 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-west-2",
+        BUCKET: "notes-app-uploads-cjmcrae"
+    },
+    apiGateway: {
+        REGION: "eu-west-2",
+        URL: "https://jwgmfuz6e8.execute-api.eu-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "eu-west-2",
+        USER_POOL_ID: "eu-west-2_15BanJzmG",
+        APP_CLIENT_ID: "1195mpfh3jvbtta2b2ut94mt05",
+        IDENTITY_POOL_ID: "eu-west-2:50e89672-08d1-4f15-b6ce-0e60a0252046"
+    }
+};
